c6b2aac4a83935e7adc40de14485384745eaf47c
Anwar Jones <anwar.jones@gmail.com>
2018-02-02 16:31:01 -0600

    Incomplete work on bios pages.  Need to add details. Also need to add bio details to employee data object.

--- src/app/about-page/about-page-bio.component.ts
+++ src/app/about-page/about-page-bio.component.ts
@@ -11,12 +11,25 @@ import { ActivatedRoute } from '@angular/router';
 export class AboutPageBioComponent implements OnInit {
   employees: IEmployee[];
   errorMessage: string;
+  selectedEmployee: IEmployee;
+  selectedId: number;
 
   constructor(private employeeService: EmployeeService,
               private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.employeeService.getEmployees().subscribe(employees => this.employees = employees, error => this.errorMessage = <any>error);
+    this.employeeService.getEmployees()
+    .subscribe(employees => this.employees = employees
+        , error => this.errorMessage = <any>error);
+    this.employeeSelect(1);
+  }
+  employeeSelect(id: number): void {
+    this.selectedId = id;
+    console.log(this.selectedId);
+    this.employeeService.getEmployee(this.selectedId)
+    .subscribe(
+      employee => this.selectedEmployee = employee
+      , error => this.errorMessage = <any>error);
   }
 
 }
