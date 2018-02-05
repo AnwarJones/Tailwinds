300a8fa3e159818a2a2b16d6bd8aff1a7e30f507
Anwar Jones <anwar.jones@gmail.com>
2018-02-04 22:40:16 -0600

    fixed video sizing, uncommented css for background vid css.  Modified css for navbar dropdown

--- src/app/backgroundvid/backgroundvid.component.ts
+++ src/app/backgroundvid/backgroundvid.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-backgroundvid',
   templateUrl: './backgroundvid.component.html'
-  //,  styleUrls: ['./backgroundvid.component.css']
+  ,styleUrls: ['./backgroundvid.component.css']
 })
 export class BackgroundvidComponent implements OnInit {
   sources: string = 'assets/560156925.mp4'
